d0f73d73871d22b09812848e668252f5adba805a
shreyasvbs <shreyasdeshpande2904@gmail.com>
2026-08-24 08:20:28 +0530

    deploy aks cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-#In Azure, all infrastructure elements such as virtual machines, storage, and our Kubernetes cluster need to be attached to a resource group.
+# In Azure, all infrastructure elements such as virtual machines, storage,
+# and our Kubernetes cluster need to be attached to a resource group.
 
 resource "azurerm_resource_group" "aks-rg" {
   name     = var.resource_group_name
   location = var.location
-
 }
 
 resource "azurerm_role_assignment" "role_acrpull" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
 
@@ -33,17 +33,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = var.system_node_count
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
-    zones  = [1, 2, 3]
+    zones               = [1, 2, 3]
     enable_auto_scaling = false
   }
 
   identity {
-
     type = "SystemAssigned"
   }
 
   network_profile {
     load_balancer_sku = "standard"
-    network_plugin    = "kubenet" 
+    network_plugin    = "kubenet"
   }
 }
