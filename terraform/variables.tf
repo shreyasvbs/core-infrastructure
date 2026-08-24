4ef47be1a114b7ebd0de5de34a3fafc8863dfb61
shreyasvbs <shreyasdeshpande2904@gmail.com>
2026-08-24 09:21:53 +0530

    Add subscription_id variable for Azure configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "RG name in Azure"
